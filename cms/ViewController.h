a8a1e7000b8514b796eabb942415a4ebd1758e54
Griffin Anderson <griffinanderson@Griffins-MacBook-Pro.local>
2015-07-23 14:20:36 -0400

    Initial Commit

---
+++ cms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cms
+//
+//  Created by Griffin Anderson on 7/23/15.
+//  Copyright (c) 2015 Griffin Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cms/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cms
+//
+//  Created by Griffin Anderson on 7/23/15.
+//  Copyright (c) 2015 Griffin Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
